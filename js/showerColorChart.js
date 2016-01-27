33b6f9367a0e0aba062a3c78d85513e5ebafaf7a
Dubem <nriagudubem@gmail.com>
2016-01-27 06:34:39 +0100

    penutinmate changes

--- js/showerColorChart.js
+++ js/showerColorChart.js
@@ -3,21 +3,21 @@
  */
 function white(){
     document["imgName"].src="media/immagini/shower tray 3.jpg";
-    document.getElementById("myLink").href = "white.php";
+    //document.getElementById("myLink").href = "white.php";
 }
 function blue(){
     document["imgName"].src="media/immagini/shower=tray-blue.jpg";
-    document.getElementById("myLink").href = "blueBathtub.html";
+    //document.getElementById("myLink").href = "blueBathtub.html";
 }
 
 function pink(){
     document["imgName"].src="media/immagini/shower-tray-pink.jpg";
-    document.getElementById("myLink").href = "pink.php";
+    //document.getElementById("myLink").href = "pink.php";
 }
 
 function peach(){
     document["imgName"].src="media/immagini/shower-tray-peach.jpg";
-    document.getElementById("myLink").href = "peach.php";
+    //document.getElementById("myLink").href = "peach.php";
 
 }
 
